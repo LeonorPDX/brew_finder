dbd6c0f6f63543c162e7ec0b9e471e7a62fdcf29
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-06 00:30:43 +0000

    Improved language and 'puts' sequence in CLI

--- lib/brew_finder/cli.rb
+++ lib/brew_finder/cli.rb
@@ -1,38 +1,37 @@
 class BrewFinder::CLI 
   
   def call 
-    welcome
     list_breweries
     brewery_details
   end
   
-  def welcome
-    puts "Welcome to Brew Finder! Let's find you some brews near you."
-  end
-  
   def list_breweries
+    puts "Welcome to Brew Finder! Let's find you some brews near you."
     puts "Please enter your zipcode."
     #gets user input, finds breweries in that area from API and formats them in numbered list 
     @breweries = BrewFinder::Brewery.breweries_nearby#(input)
     
     @breweries.each.with_index(1) {|b, i| puts "#{i}) #{b.name} - #{b.street} - #{b.type}"}
-      
   end
   
   def brewery_details
     input = nil 
 
-    puts "Which brewery would you like to learn more about? Please enter a number. Or, type 'new zip' to try a new location or type 'exit'"
+    puts "Which brewery would you like to learn more about? Please enter a number, or 'new zip' to try a new location or 'exit'." 
     
     while input != "exit"
-     input = gets.strip.downcase
+    input = gets.strip.downcase
       if input.to_i > 0 
-        puts @breweries[input.to_i-1]
+        b = @breweries[input.to_i-1]
+        puts "Here's more detailed information about #{b.name}."
       elsif input == "new zip"
         list_breweries
+      elsif input == "exit"
+        goodbye
       else
         puts "Not sure what you meant, please type 'new zip' or 'exit'"
       end
+    puts "You can pick another number from the list, type 'new zip' or 'exit'."
      end
   end
   
