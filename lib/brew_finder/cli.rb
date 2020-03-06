6414ca467e988db4ddb5f453944b8fcf7ed4165c
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-06 00:01:23 +0000

    Got the list_breweries and brewery_details methods working together

--- lib/brew_finder/cli.rb
+++ lib/brew_finder/cli.rb
@@ -15,22 +15,17 @@ class BrewFinder::CLI
     #gets user input, finds breweries in that area from API and formats them in numbered list 
     @breweries = BrewFinder::Brewery.breweries_nearby#(input)
     
-    @breweries.each.with_index(1) do |brewery, i|
-      puts "#{i}) #{brewery.name} - #{brewery.street} - #{brewery.type}"
-    end
+    @breweries.each.with_index(1) {|b, i| puts "#{i}) #{b.name} - #{b.street} - #{b.type}"}
       
   end
   
   def brewery_details
     input = nil 
 
-    
     puts "Which brewery would you like to learn more about? Please enter a number. Or, type 'new zip' to try a new location or type 'exit'"
     
-    while input != exit
-    
-    input = gets.strip.downcase
-
+    while input != "exit"
+     input = gets.strip.downcase
       if input.to_i > 0 
         puts @breweries[input.to_i-1]
       elsif input == "new zip"
@@ -38,7 +33,6 @@ class BrewFinder::CLI
       else
         puts "Not sure what you meant, please type 'new zip' or 'exit'"
       end
-      
      end
   end
   
