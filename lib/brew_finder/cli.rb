928d74f565de4b127be38a0cb9012eb2f17ba7b1
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-07 20:10:18 +0000

    Changed CLI to have cleaner methods with single function

--- lib/brew_finder/cli.rb
+++ lib/brew_finder/cli.rb
@@ -26,7 +26,7 @@ class BrewFinder::CLI
   end
   
   def search_breweries
-    puts "Would you like to see all the breweries in your state (up to 50),"
+    puts "Would you like to see a selection of 50 breweries in your state,"
     puts "or all the breweries in your zip code? Please type 'zip' or 'state'."
     choice = gets.strip.downcase
     
@@ -34,37 +34,44 @@ class BrewFinder::CLI
       puts "Please enter your 5-digit zipcode."
       input = gets.strip.to_i
       BrewFinder::API.breweries_zip(input)
+      display_zip
     elsif choice == "state"
       puts "Please enter the full, unabbreviated name of your state."
       input = gets.strip.downcase.gsub(" ","_")
       BrewFinder::API.breweries_state(input)
+      display_state
     elsif choice == "exit"
       goodbye
     else
       puts "Please type either 'zip' or state'."
     end
+  end
     
+  def display_zip
     puts ""
     BrewFinder::Brewery.all.each.with_index(1) {|b, i| puts "#{i}) #{b.name} - #{b.street} - #{b.brewery_type}"}
     puts ""
-
+  end
+  
+  def display_state
+    puts ""
+    BrewFinder::Brewery.all.each.with_index(1) {|b, i| puts "#{i}) #{b.name} - #{b.city} - #{b.brewery_type}"}
+    puts ""
   end
   
   def brewery_details
     input = nil 
-    
+    while input != "exit"
     puts "Which brewery would you like to learn more about? Please enter a number, or 'new location' to search again or 'exit'." 
     
-    while input != "exit"
     input = gets.strip.downcase
-      if input.to_i > 0 && input.to_i <= BrewFinder::Brewery.all.length
+      if input == "exit"
+        goodbye
+      elsif input.to_i > 0 && input.to_i <= BrewFinder::Brewery.all.length
         BrewFinder::Brewery.display_details(input.to_i-1)
       elsif input == "new location"
         BrewFinder::Brewery.destroy_all
         search_breweries
-        puts "Do you want to learn more about one of these? Enter a number, 'new location' or 'exit'."
-      elsif input == "exit"
-        goodbye
       else
         puts "Not sure what you meant..."
         puts "Please pick a number from the list, or type 'new location' or 'exit'"
